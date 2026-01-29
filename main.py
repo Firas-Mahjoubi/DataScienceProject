a75cc1c484b3453126c57fd4a3bdd3a79d808bde
Firas-Mahjoubi <mahjoubi.firas@esprit.tn>
2026-01-29 16:31:24 +0100

    data ingestion pipeline

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from src.DataScienceProject import logger
-logger.info("Welcome to our custom logging datascience")
+from src.DataScienceProject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
