416c9047e4842c4a839985496fb5c09eb5c5fb4d
Firas-Mahjoubi <mahjoubi.firas@esprit.tn>
2026-01-30 23:30:00 +0100

    data validation complited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.DataScienceProject import logger
 from src.DataScienceProject.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+from src.DataScienceProject.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 
 
 STAGE_NAME = "Data Ingestion Stage"
@@ -7,6 +8,16 @@ try:
     logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
     data_ingestion=DataIngestionTrainingPipeline()
     data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.initiate_data_validation()
     logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
